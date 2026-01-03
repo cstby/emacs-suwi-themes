8850ea66d7bd56e0ec69e6782c33fd98da649e4f
Carl Steib <carl.steib@gmail.com>
2026-01-03 12:26:42 -0600

    Add reference file

---
+++ reference.el
@@ -0,0 +1,122 @@
+'(
+  ;; HSL (X 100 59)
+  (red-cooler "#ff2e62")                ; 345
+  (red "#ff3030")                       ; 0
+  (red-warmer "#ff622e")                ; 15
+  (yellow-warmer "#ff962e")             ; 30
+  (yellow "#ffff2e")                    ; 60
+  (yellow-cooler "#cbff2e")             ; 75
+  (green-warmer "#96ff2e")              ; 90
+  (green "#2eff2e")                     ; 120
+  (green-cooler "#2eff96")              ; 150
+  (cyan-warmer "#2effcb")               ; 165
+  (cyan "#2effff")                      ; 180
+  (cyan-cooler "#2ecbff")               ; 195
+  (blue-warmer "#2e96ff")               ; 210
+  (blue "#2e2eff")                      ; 240
+  (blue-cooler "#622eff")               ; 255
+  (magenta-warmer "#cb2eff")            ; 285
+  (magenta "#ff2eff")                   ; 300
+  (magenta-cooler "#ff2e96"))           ; 330
+
+'(
+  ;; HSL (X 100 59) - evenly spaced
+  (red              "#ff2e2e")          ;   0°
+  (red-warmer       "#ff732e")          ;  20°
+  (orange           "#ffb92e")          ;  40°
+  (yellow           "#ffff2e")          ;  60°
+  (yellow-cooler    "#b9ff2e")          ;  80°
+  (yellow-green     "#73ff2e")          ; 100°
+  (green            "#2eff2e")          ; 120°
+  (green-cooler     "#2eff73")          ; 140°
+  (cyan-warmer      "#2effb9")          ; 160°
+  (cyan             "#2effff")          ; 180°
+  (cyan-cooler      "#2eb9ff")          ; 200°
+  (blue-warmer      "#2e73ff")          ; 220°
+  (blue             "#2e2eff")          ; 240°
+  (blue-cooler      "#732eff")          ; 260°
+  (violet           "#b92eff")          ; 280°
+  (magenta          "#ff2eff")          ; 300°
+  (magenta-cooler   "#ff2eb9")          ; 320°
+  (red-cooler       "#ff2e73")          ; 340°
+
+
+  )
+
+(
+ (
+  ;; HSL (X 60 55) - evenly spaced
+  (red              "#d14747")          ;   0°
+  (red-warmer       "#d17547")          ;  20°
+  (yellow-warmer    "#d1a347")          ;  40°
+  (yellow           "#d1d147")          ;  60°
+  (yellow-cooler    "#a3d147")          ;  80°
+  (green-warmer     "#75d147")          ; 100°
+  (green            "#47d147")          ; 120°
+  (green-cooler     "#47d175")          ; 140°
+  (cyan-warmer      "#47d1a3")          ; 160°
+  (cyan             "#47d1d1")          ; 180°
+  (cyan-cooler      "#47a3d1")          ; 200°
+  (blue-warmer      "#4775d1")          ; 220°
+  (blue             "#4747d1")          ; 240°
+  (blue-cooler      "#7547d1")          ; 260°
+  (magenta-warmer   "#a347d1")          ; 280°
+  (magenta          "#d147d1")          ; 300°
+  (magenta-cooler   "#d147a3")          ; 320°
+  (red-cooler       "#d14775")          ; 340°
+  )
+
+
+ )
+
+(
+ (
+  ;; HSL (X 60 55) - evenly spaced
+  (red              "#d14747")          ;   0°
+  (orange           "#d17547")          ;  20°
+  (amber            "#d1a347")          ;  40°
+  (yellow           "#d1d147")          ;  60°
+  (lime             "#a3d147")          ;  80°
+  (grass            "#75d147")          ; 100°
+  (green            "#47d147")          ; 120°
+  (emerald          "#47d175")          ; 140°
+  (aquamarine       "#47d1a3")          ; 160°
+  (cyan             "#47d1d1")          ; 180°
+  (celeste          "#47a3d1")          ; 200°
+  (cerulean         "#4775d1")          ; 220°
+  (blue             "#4747d1")          ; 240°
+  (ultramarine      "#7547d1")          ; 260°
+  (purple           "#a347d1")          ; 280°
+  (magenta          "#d147d1")          ; 300°
+  (fuchsia          "#d147a3")          ; 320°
+  (rose             "#d14775")          ; 340°
+  )
+
+
+ )
+
+(
+ (
+  ;; HSL (X 60 55) - evenly spaced
+  (red              "#d14747")          ;   0°
+  (red-warmer       "#d17547")          ;  20°
+  (yellow-warmer    "#d1a347")          ;  40°
+  (yellow           "#d1d147")          ;  60°
+  (yellow-cooler    "#a3d147")          ;  80°
+  (green-warmer     "#75d147")          ; 100°
+  (green            "#47d147")          ; 120°
+  (green-cooler     "#47d175")          ; 140°
+  (cyan-cooler      "#47d1a3")          ; 160°
+  (cyan             "#47d1d1")          ; 180°
+  (cyan-warmer      "#47a3d1")          ; 200°
+  (blue-cooler      "#4775d1")          ; 220°
+  (blue             "#4747d1")          ; 240°
+  (blue-warmer      "#7547d1")          ; 260°
+  (magenta-cooler   "#a347d1")          ; 280°
+  (magenta          "#d147d1")          ; 300°
+  (magenta-warmer   "#d147a3")          ; 320°
+  (red-cooler       "#d14775")          ; 340°
+  )
+
+
+ )
